b1ebd0ce1648b34ae9b0a30212b1b2c914f178c8
Sakthiram-R <sakthivani002@gmail.com>
2026-02-16 13:55:30 +0530

    Added vercel URL

--- backend/taskproject/settings.py
+++ backend/taskproject/settings.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    
     # Railway & Vercel URLs will be added as env variables
     os.getenv('FRONTEND_URL', ''),  # Set this on Railway/Vercel
 ]
